56b1a6f18ff2ea945df3b1907c14b262d6269c7b
John <john.a.cowie@gmail.com>
2020-08-07 09:35:48 +0100

    Support nested vars

--- src/Env.purs
+++ src/Env.purs
@@ -21,6 +21,7 @@ import Node.Process (getEnv)
 import Chalk as Chalk
 import Effect (Effect)
 import Effect.Console as Console
+import Effect.Exception.Unsafe (unsafeThrow)
 
 type Env = Object String
 
@@ -103,10 +104,13 @@ else instance readValueAll :: (ParseValue t) => ReadValue t where
 readValueFromEnv :: forall t. (ReadValue t) => Var t -> Object String -> Either EnvError t
 readValueFromEnv v@(Var {varName, default}) env = readValue (varInfo v) default $ lookup varName env
 
-class Compiler (el :: RowList) (rl :: RowList) (e :: # Type) (r :: # Type) | el -> rl, e -> r where
+-- TODO rename this typeclass
+class Compiler (el :: RowList) (rl :: RowList) (e :: # Type) (r :: # Type) where
   compileParser :: forall proxy. proxy el -> proxy rl -> (Record e) -> Object String -> Either EnvError (Record r)
 
-instance compilerCons ::
+instance compilerResultsNil :: (TypeEquals {} (Record r)) => Compiler pl Nil p r where
+  compileParser _ _ _ _ = pure $ to {}
+else instance compilerCons ::
   ( IsSymbol l
   , ReadValue t
   , Row.Lacks l rt
@@ -126,9 +130,31 @@ instance compilerCons ::
 
             insert (Left valueErr) (Left tailErrs) = Left $ valueErr <> tailErrs
             insert valE tailE = Record.insert name <$> valE <*> tailE
+else instance compilerConsSubVars ::
+  ( IsSymbol l
+  , Row.Lacks l rt
+  , Row.Lacks l pt
+  , ListToRow rlt rt
+  , ListToRow plt pt
+  , RowToList v vlt
+  , RowToList t tlt
+  , ListToRow vlt v
+  , ListToRow tlt t
+  , ReadEnv v t
+  , Row.Cons l (Record v) pt p
+  , Row.Cons l (Record t) rt r
+  , Compiler plt rlt pt rt
+  , Compiler vlt tlt v t
+  ) => Compiler (Cons l (Record v) plt) (Cons l (Record t) rlt) p r where
+    compileParser _ _ vars env = insert value tail
+      where name = (SProxy :: SProxy l)
+            (subVars :: Record v) = Record.get name vars
+            value = readEnv subVars env  -- replace with readEnv?
+            varsTail = Record.delete name vars
+            tail = compileParser (RLProxy :: RLProxy plt) (RLProxy :: RLProxy rlt) varsTail env
 
-instance compilerNil :: (TypeEquals {} (Record r), TypeEquals {} (Record p)) => Compiler Nil Nil p r where
-  compileParser _ _ _ _ = pure $ to {}
+            insert (Left valueErr) (Left tailErrs) = Left $ valueErr <> tailErrs
+            insert valE tailE = Record.insert name <$> valE <*> tailE
 
 class ReadEnv (e :: # Type) (r :: # Type) where
   readEnv :: (Record e) -> Object String -> Either EnvError (Record r)
@@ -142,19 +168,33 @@ instance readEnvImpl ::
   ) => ReadEnv e r where
     readEnv = compileParser (RLProxy :: RLProxy el) (RLProxy :: RLProxy rl)
 
-exampleParser2 :: Object String -> Either EnvError {a :: Int, c :: Int, b :: String, d :: Maybe String}
-exampleParser2 = readEnv { a: var "BILL" # describe "Bill is an int" # defaultTo 7
-                         , b: var "BEN" # describe "Ben is a string"
-                         , c: var "BOB_BOB" # describe "Bob bob is the feckin best"
-                         , d: var "MAYBE" # describe "Value is optional"}
+-- TODO type class with instances for var and record, so that sub record can also be processed
+-- TODO  maybe just need another instance of Compiler where instead of Var t value, it's  a ReadEnv value?
 
--- TODO function for merging parsers
+exampleEnv :: { a :: Var Int
+              , b :: Var String
+              , c :: {c1 :: Var Int, c2 :: Var Int}
+              , d :: Var (Maybe String)}
+exampleEnv = { a: var "BILL" # describe "Bill is an int" # defaultTo 7
+             , b: var "BEN" # describe "Ben is a string"
+             , c: {c1: var "C1", c2: var "C2"}
+             , d: var "MAYBE" # describe "Value is optional"}
+
+-- TODO Should you be able to read a subset of values?  means the result type declaration is required when using..
+exampleParser :: Object String -> Either EnvError {a :: Int, b :: String, c :: {c1 :: Int, c2 :: Int}}
+exampleParser = readEnv exampleEnv
+
+blah :: {a :: Int, b :: String}
+blah = Record.merge {a: 1} {b: ""}
+
+-- TODO function for merging parsers  (basically Record.Merge)
 -- TODO how to document parsers?
 -- TODO can support nested?
+-- TODO support parser override? (supply parser to readMaybeValue)
 
 main :: Effect Unit
 main = do
   env <- getEnv
-  case exampleParser2 env of
+  case exampleParser env of
     (Left err) -> Console.error (printErrorForConsole err)
     (Right val) -> Console.log (show val)
